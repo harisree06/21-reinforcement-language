4d67f8599add5e7d1a2fa524d65ad8a5d5c44bad
Lan Phan <lanc_phan@hotmail.com>
2017-10-10 14:19:08 -0400

    The key to the first round of the hash is done.

--- language.rb
+++ language.rb
@@ -1,3 +1,14 @@
 digits = ['1','2','3','4','5','6','7','8','9']
 en = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 fr = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf']
+
+counter = 0
+
+length = digits.length
+
+h = {}
+
+digits.each do |digit|
+  h[digit] = digit
+end
+puts h
